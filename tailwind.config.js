cc0b3e82c5838f59c250872aaed70d9854b01bf2
Spardutti <luisdamian.sp@gmail.com>
2024-11-17 10:12:43 -0300

    feat(set up): react query, colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				colors: {
+					background: '#0D1B2A',
+					'background-alt': '#1B263B',
+					primary: '#415A77',
+					secondary: '#778DA9',
+					text: '#E0E1DD',
+				},
+			},
+		},
 	},
 	plugins: [],
 };
