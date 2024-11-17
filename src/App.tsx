eba3e7e487fe1680e47a7cbbcfc738ce57841631
Spardutti <luisdamian.sp@gmail.com>
2024-11-17 09:51:35 -0300

    husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,8 @@
-import { useState } from 'react';
-
 const App = () => {
-	const [count, setCount] = useState(0);
-
 	return (
-		<>
-			<div>
-				<a
-					href="https://vite.dev"
-					target="_blank"
-				>
-					<img
-						alt="Vite logo"
-						className="logo"
-					/>
-				</a>
-				<a
-					href="https://react.dev"
-					target="_blank"
-				>
-					<img
-						alt="React logo"
-						className="logo react"
-					/>
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-		</>
+		<div>
+			<div className="size-10 bg-red-500">Tailwind set up</div>
+		</div>
 	);
 };
 
