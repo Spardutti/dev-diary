537d8d8bd4a97a316a95f69155c040864260d0a4
Spardutti <luisdamian.sp@gmail.com>
2024-12-24 07:27:43 -0300

    feat(daily notes): group pagination by month

--- src/features/dailyNotes/components/DailyNotes/DailyNotes.tsx
+++ src/features/dailyNotes/components/DailyNotes/DailyNotes.tsx
@@ -19,45 +19,58 @@ const DailyNotes = () => {
 
 	if (isLoading || !dailyNotes) return null;
 
+	const mergedNotesByMonth = () => {
+		// Combine notes from all pages into a single object grouped by month
+		const groupedData: Record<string, IDailyNote[]> = formatPaginationList(dailyNotes).reduce((acc, page) => {
+			Object.entries(page).forEach(([month, notes]) => {
+				if (!acc[month]) {
+					acc[month] = [];
+				}
+				acc[month] = [...acc[month], ...notes]; // Merge notes for the same month
+			});
+			return acc;
+		}, {});
+
+		return groupedData;
+	};
+
 	return (
 		<ScrollArea
 			className="flex flex-col gap-2 p-4 md:p-6 h-0 flex-grow"
 			ref={observerRef}
 		>
-			{formatPaginationList(dailyNotes).map((entry) => {
-				return Object.entries(entry).map(([key, value]) => {
-					const month = dayjs(key).format('MMM YY');
+			{Object.entries(mergedNotesByMonth()).map(([key, value]) => {
+				const month = dayjs(key).format('MMM YY');
 
-					return (
-						<div
-							className="flex flex-col gap-2"
-							key={key}
-						>
-							<p> {month}</p>
-							<div className="flex flex-wrap gap-2">
-								{value.map((note: IDailyNote) => {
-									return (
-										<Link
-											className="h-28"
-											key={note.id}
-											to="/projects/$projectId/daily-notes/$noteId"
-											params={{ noteId: note.id, projectId: projectId }}
-										>
-											<Card className="w-64 h-full hover:bg-background-alt/60">
-												<CardHeader>
-													<CardTitle className="text-secondary">{notesFrom(note.date)}</CardTitle>
-												</CardHeader>
-												<CardContent>
-													<p className="line-clamp-1 text-text">{cleanHtml(note.note)}</p>
-												</CardContent>
-											</Card>
-										</Link>
-									);
-								})}
-							</div>
+				return (
+					<div
+						className="flex flex-col gap-2"
+						key={key}
+					>
+						<p> {month}</p>
+						<div className="flex flex-wrap gap-2">
+							{value.map((note: IDailyNote) => {
+								return (
+									<Link
+										className="h-28"
+										key={note.id}
+										to="/projects/$projectId/daily-notes/$noteId"
+										params={{ noteId: note.id, projectId: projectId }}
+									>
+										<Card className="w-64 h-full hover:bg-background-alt/60">
+											<CardHeader>
+												<CardTitle className="text-secondary">{notesFrom(note.date)}</CardTitle>
+											</CardHeader>
+											<CardContent>
+												<p className="line-clamp-1 text-text">{cleanHtml(note.note)}</p>
+											</CardContent>
+										</Card>
+									</Link>
+								);
+							})}
 						</div>
-					);
-				});
+					</div>
+				);
 			})}
 		</ScrollArea>
 	);
