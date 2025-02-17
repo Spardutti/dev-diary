a20dc804301a7f4163a7dc60f4716a507d23c5b1
Spardutti <luisdamian.sp@gmail.com>
2025-02-17 19:00:00 -0300

    fix: tanstack dev tools on production

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -12,7 +12,7 @@ export const Route = createRootRouteWithContext<RouterContext>()({
 	component: () => (
 		<>
 			<Outlet />
-			<TanStackRouterDevtools />
+			{import.meta.env.VITE_API_NODE_ENV === 'develop' && <TanStackRouterDevtools />}
 		</>
 	),
 });
