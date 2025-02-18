9316633524718b1d4e4a2adbf29661b628a1cb93
Spardutti <luisdamian.sp@gmail.com>
2025-02-18 17:35:17 -0300

    feat(todo): create todo tooltip

--- src/features/todos/components/CreateTodoForm/CreateTodoForm.tsx
+++ src/features/todos/components/CreateTodoForm/CreateTodoForm.tsx
@@ -1,12 +1,16 @@
 import { Input } from '@/components/ui/input';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
 import { useCreateTodo } from '@/features/todos/api/todosQueries';
 import { useParams } from '@tanstack/react-router';
 import React, { useState } from 'react';
 
 const CreateTodoForm = () => {
 	const [todoTitle, setTodoTitle] = useState('');
+	const [isOpen, setIsOpen] = useState(false);
+
 	const { projectId } = useParams({ strict: false });
-	const { mutateAsync: createTodo } = useCreateTodo();
+
+	const { mutateAsync: createTodo, isPending } = useCreateTodo();
 
 	const onEnter = async (e: React.KeyboardEvent<HTMLInputElement>) => {
 		if (e.key === 'Enter') {
@@ -22,12 +26,22 @@ const CreateTodoForm = () => {
 
 	return (
 		<div>
-			<Input
-				placeholder="What needs doing?"
-				onKeyDown={onEnter}
-				value={todoTitle}
-				onChange={(e) => setTodoTitle(e.target.value)}
-			/>
+			<TooltipProvider>
+				<Tooltip open={isOpen}>
+					<TooltipTrigger asChild>
+						<Input
+							disabled={isPending}
+							placeholder="What needs doing?"
+							onKeyDown={onEnter}
+							value={todoTitle}
+							onChange={(e) => setTodoTitle(e.target.value)}
+							onFocus={() => setIsOpen(true)}
+							onBlur={() => setIsOpen(false)}
+						/>
+					</TooltipTrigger>
+					<TooltipContent>Press enter to save changes</TooltipContent>
+				</Tooltip>
+			</TooltipProvider>
 		</div>
 	);
 };
