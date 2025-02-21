0e9dc0b483d235a46e66a20454829197c4b1f94b
Spardutti <luisdamian.sp@gmail.com>
2025-02-21 14:18:08 -0300

    feat(snippet): action menu

---
+++ src/features/snippets/components/SnippetActions/SnippetActions.tsx
@@ -0,0 +1,63 @@
+import { Dialog } from '@/components/ui/dialog';
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { DeleteSnippetModalContent } from '@/features/snippets/components/DeleteSnippetModal/DeleteSnippetModal';
+import type { ISnippet } from '@/features/snippets/types/ISnippet';
+import { Edit, Trash } from 'lucide-react';
+import React, { useState } from 'react';
+
+interface SnippetActionsProps {
+	trigger: React.ReactNode;
+	snippet: ISnippet;
+}
+
+const SnippetActions = ({ trigger, snippet }: SnippetActionsProps) => {
+	const [isOpen, setIsOpen] = useState(false);
+	const [dialogOpen, setDialogOpen] = useState<'delete' | 'edit' | null>(null);
+
+	const openDialog = (dialog: 'delete' | 'edit') => {
+		setDialogOpen(dialog);
+		setIsOpen(true);
+	};
+
+	const closeDialog = () => {
+		setDialogOpen(null);
+		setIsOpen(false);
+	};
+
+	return (
+		<Dialog
+			open={isOpen}
+			onOpenChange={setIsOpen}
+		>
+			<DropdownMenu>
+				<DropdownMenuTrigger> {trigger} </DropdownMenuTrigger>
+				<DropdownMenuContent>
+					<DropdownMenuItem onClick={() => openDialog('edit')}>
+						<Edit /> Edit
+					</DropdownMenuItem>
+
+					<DropdownMenuItem
+						onClick={() => openDialog('delete')}
+						className="w-full focus:bg-red-500 focus:text-text"
+					>
+						<Trash /> Delete
+					</DropdownMenuItem>
+				</DropdownMenuContent>
+			</DropdownMenu>
+
+			{dialogOpen === 'delete' && (
+				<DeleteSnippetModalContent
+					snippet={snippet}
+					closeDialog={closeDialog}
+				/>
+			)}
+		</Dialog>
+	);
+};
+
+export default SnippetActions;
