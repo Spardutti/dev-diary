7da732f0502fbb8546e4255649485b285e73afee
Spardutti <luisdamian.sp@gmail.com>
2025-02-21 14:16:28 -0300

    style: textarea and select

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -16,7 +16,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, React.ComponentProps<'tex
 		return (
 			<textarea
 				className={cn(
-					'flex min-h-[80px] w-full max-h-[300px] overflow-x-auto rounded-md outline-none border-background-alt focus-within:bg-background-alt bg-transparent px-3 py-2 text-base resize-none focus-visible:outline-none focus-visible:ring-neutral-950 placeholder:text-neutral-500   focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm dark:border-neutral-800 dark:bg-neutral-950 dark:ring-offset-neutral-950 dark:placeholder:text-neutral-400 dark:focus-visible:ring-neutral-300',
+					'flex min-h-[80px] w-full max-h-[300px] border border-background-alt overflow-x-auto rounded-md outline-none focus-within:bg-background-alt bg-transparent px-3 py-2 text-base resize-none focus-visible:outline-none focus-visible:ring-neutral-950 placeholder:text-neutral-500   focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm dark:border-neutral-800 dark:bg-neutral-950 dark:ring-offset-neutral-950 dark:placeholder:text-neutral-400 dark:focus-visible:ring-neutral-300',
 					className,
 				)}
 				onChange={handleChange}
