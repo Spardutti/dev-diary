5865eab79d73c8c73bb9b9ad520d7cb22582fd40
Spardutti <luisdamian.sp@gmail.com>
2025-02-28 15:37:58 -0300

    style: checkbox

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -11,7 +11,7 @@ const Checkbox = React.forwardRef<
 	<CheckboxPrimitive.Root
 		ref={ref}
 		className={cn(
-			'peer h-4 w-4 shrink-0 rounded-sm border border-separator shadow focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-neutral-950 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-text dark:border-neutral-800  dark:focus-visible:ring-neutral-300 dark:data-[state=checked]:bg-primary dark:data-[state=checked]:text-neutral-900',
+			'peer h-4 w-4 shrink-0 rounded-sm border border-primary shadow focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-neutral-950 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-text dark:border-neutral-800  dark:focus-visible:ring-neutral-300 dark:data-[state=checked]:bg-primary dark:data-[state=checked]:text-neutral-900',
 			className,
 		)}
 		{...props}
