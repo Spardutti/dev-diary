11e33fdc1e739e2d744c085a6589b7f6329f154b
Spardutti <luisdamian.sp@gmail.com>
2025-02-28 15:28:10 -0300

    feat(avatar): include in header retro avatar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,13 @@
-import { LogOut } from 'lucide-react';
 import { Link, useParams } from '@tanstack/react-router';
-import { useLogout } from '@/features/auth/api/authQueries';
 import MobileNavigation from '@/components/MobileNavigation';
 import ProjectSelectorDropDown from '@/features/projects/components/ProjectSelectorDropdown';
 
+import AvatarDropdown from '@/features/dashboard/components/AvatarDropdown';
+
 const Header = () => {
-	const { mutateAsync: logout } = useLogout();
 	const { projectId } = useParams({ strict: false });
 
-	const onLogout = async () => {
-		await logout();
-	};
-
 	return (
-		// <header className="sticky top-0 z-50 flex h-16 flex-shrink-0 items-center text-green-400 border-separator bg-background/95 px-4 backdrop-blur supports-[backdrop-filter]:bg-background/60 border-b border-green-900/60 p-2   justify-between  ">
 		<header className="border-b border-green-900/60 h-16 p-2 flex justify-between items-center">
 			<div className="flex items-center gap-2 h-full">
 				<MobileNavigation />
@@ -34,12 +28,7 @@ const Header = () => {
 				</div>
 			</div>
 			<div className="flex items-center gap-4">
-				<div
-					onClick={onLogout}
-					className="text-green-400 hover:text-green-300 transition-colors cursor-pointer"
-				>
-					<LogOut className="h-5 w-5" />
-				</div>
+				<AvatarDropdown />
 			</div>
 		</header>
 	);
---
+++ src/features/dashboard/components/AvatarDropdown/AvatarDropdown.tsx
@@ -0,0 +1,50 @@
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { useAuth } from '@/context/useAuth';
+import { useLogout } from '@/features/auth/api/authQueries';
+import { Avatar, AvatarFallback } from '@radix-ui/react-avatar';
+import { LogOut } from 'lucide-react';
+
+const AvatarDropdown = () => {
+	const user = useAuth();
+	const { mutateAsync: logout } = useLogout();
+
+	const onLogout = async () => {
+		await logout();
+	};
+
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger>
+				<div className="bg-black p-4 flex items-center gap-3">
+					<Avatar className="w-10 h-10 text-sm bg-black border-2 border-green-500/80 rounded-none flex items-center justify-center relative overflow-visible">
+						<AvatarFallback className="bg-black text-green-500 font-mono">
+							{user.profile?.name.charAt(0)}
+						</AvatarFallback>
+
+						<div className="absolute -top-1 -left-1 w-2 h-2 bg-green-500 rounded-none" />
+						<div className="absolute -top-1 -right-1 w-2 h-2 bg-green-500 rounded-none" />
+						<div className="absolute -bottom-1 -left-1 w-2 h-2 bg-green-500 rounded-none" />
+						<div className="absolute -bottom-1 -right-1 w-2 h-2 bg-green-500 rounded-none" />
+					</Avatar>
+					<div className="text-green-500 font-mono">
+						<div className="text-xs opacity-70">[USER_ID:1337]</div>
+						<div>{user.profile?.name}</div>
+					</div>
+				</div>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent>
+				<DropdownMenuItem onClick={onLogout}>
+					<LogOut className="h-5 w-5" />
+					Disconnect
+				</DropdownMenuItem>
+			</DropdownMenuContent>
+		</DropdownMenu>
+	);
+};
+
+export default AvatarDropdown;
