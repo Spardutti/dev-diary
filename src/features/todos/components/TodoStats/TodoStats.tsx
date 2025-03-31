0c7d47daa2945cbb626e3f44a26267ff128da25d
Spardutti <luisdamian.sp@gmail.com>
2025-03-31 16:39:40 -0300

    fix(todos): stats filter

--- src/features/todos/components/TodoStats/TodoStats.tsx
+++ src/features/todos/components/TodoStats/TodoStats.tsx
@@ -7,14 +7,16 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/com
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { TodoChart } from '@/features/todos/components/TodoChart';
 import { Skeleton } from '@/components/ui/skeleton';
+import { useParams } from '@tanstack/react-router';
 
 const today = dayjs();
 
 const TodoStats = () => {
 	const [timeRange, setTimeRange] = useState('90');
+	const { projectId } = useParams({ from: '/_authenticated/projects/$projectId/stats' });
 
 	const { data: todos, isPending } = useGetTodos(
-		`from=${today.subtract(Number(timeRange), 'day').toISOString()}&to=${today.toISOString()}&limit=200`,
+		`projectId=${projectId}&from=${today.subtract(Number(timeRange), 'day').toISOString()}&to=${today.toISOString()}&limit=200`,
 	);
 
 	const chartData = useMemo(() => {
@@ -41,7 +43,6 @@ const TodoStats = () => {
 				completed: dateCounts[date].completed,
 			}));
 
-			console.log('Formatted Chart Data:', formattedData);
 			return formattedData.sort((a, b) => a.date.localeCompare(b.date));
 		}
 
@@ -62,7 +63,7 @@ const TodoStats = () => {
 					<CardTitle>Task Completion Trends</CardTitle>
 					<CardDescription>Analyzing todo creation and completion rates over time</CardDescription>
 				</div>
-				<div className="flex gap-1">
+				<div className="md:flex-row flex-col flex gap-1">
 					<div className="flex gap-4">
 						<div className="bg-black/40 border border-green-900/50 rounded-md px-4 py-2">
 							<div className="text-xs text-green-400/70">Total Created</div>
@@ -75,7 +76,7 @@ const TodoStats = () => {
 						<div className="bg-black/40 border border-green-900/50 rounded-md px-4 py-2">
 							<div className="text-xs text-green-400/70">Completion %</div>
 							<div className="text-xl font-bold text-green-500">
-								{Math.round((totalCompleted / totalCreated) * 100)}%
+								{totalCompleted > 0 || totalCreated > 0 ? `${Math.round((totalCompleted / totalCreated) * 100)}%` : 0}
 							</div>
 						</div>
 					</div>
